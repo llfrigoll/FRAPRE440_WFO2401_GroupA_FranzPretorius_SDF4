418e6bdbd8d58caf07a1b7504d5120c1bccea43c
Franz <franzj.pretorius.2003@gmail.com>
2024-01-26 12:33:22 +0200

    Inserted comments and changed margins

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+//Creating all the variables necessary
+//Card variables, Boolean variables for outcomes and the sum for cards drawn
+//Message variable for the outcome message
+//El variables for the html elements
 let firstCard = 10
 let secondCard = 4
 let sum = firstCard + secondCard
@@ -8,9 +12,12 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+//Updates the html with the correct info
 function startGame() {
     cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
     sumEl.textContent = "Sum: " + sum
+    //Updates message pertaining on the sum value
+    //Sets won and lost conditions as booleans
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
@@ -23,6 +30,7 @@ function startGame() {
     messageEl.textContent = message
 }
 
+//Draws a new card and adds it to sum
 function newCard() {
     console.log("Drawing a new card from the deck!")
 
